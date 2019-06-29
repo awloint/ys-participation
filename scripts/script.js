8d3008db12c8deff665286a45e5f84cdcf6c4f38
Benson Imoh,ST <benson@stbensonimoh.com>
2019-06-29 19:12:22 +0100

    Add Boostrap validation to the form
     - on submit, the button color will change because Bootstrap
    class btn-danger will be replaced by btn-primary
    
     - a spinner will be added by a font-awesome class

--- scripts/script.js
+++ scripts/script.js
@@ -31,4 +31,32 @@ document.addEventListener('DOMContentLoaded', e => {
 
     val = val.toLowerCase()
   }
+
+//   Submit the form
+const form = document.querySelector('form')
+
+// On Form Submit
+form.addEventListener('submit', e => {
+    let forms = document.getElementsByClassName('needs-validation')
+
+    // Check to see if form has validation errors
+    let validation = Array.prototype.filter.call(forms, form => {
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    })
+
+    // If form doesn't have validation errors
+    if (form.checkValidity() === true) {
+        e.preventDefault()
+
+        // change the button color and add the loading class
+        document.querySelector('button').classList.remove('btn-danger')
+        document.querySelector('button').classList.add('btn-primary')
+        document.querySelector('button').innerHTML = 'Loading <span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>'
+    }
+})
 })

18a0436beda620710d5372f0b5d2e63549de7eca
Benson Imoh,ST <benson@stbensonimoh.com>
2019-06-29 18:03:52 +0100

    Add new changes to the frontend
     * Add dynamic year to the footer of the form
    
     * change the typed value of the first letter of input
    fields to uppercase

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,32 @@
 particlesJS.load('particles-js', 'scripts/particles.json', function () {
   console.log('callback - particles.js config loaded')
 })
+
+document.addEventListener('DOMContentLoaded', e => {
+  let date = new Date()
+  const year = date.getFullYear()
+  let yearContent = document.querySelector('#year')
+  yearContent.innerHTML = year
+
+  // Change the typed value of the first letter to uppercase
+  document.querySelector('#firstName').onchange = e => {
+    let val = document.querySelector('#firstName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#lastName').onchange = e => {
+    let val = document.querySelector('#lastName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#email').onchange = e => {
+    let val = document.querySelector('#email').value
+    RegExp = /\b[a-z]/g
+
+    val = val.toLowerCase()
+  }
+})

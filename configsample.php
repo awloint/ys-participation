ed5f8660f7e0b607c16dcd1783fd727fc75e3574
Benson Imoh,ST <benson@stbensonimoh.com>
2019-06-30 18:26:25 +0100

    Add sample configuration file and sample database

---
+++ configsample.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This is the Server configuration script
+ *
+ * PHP version 7.2
+ *
+ * @category Server_Configuration
+ * @package  Server_Configuration
+ * @author   Benson Imoh,ST <benson@stbensonimoh.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://stbensonimoh.com
+ */
+// Database Configuration
+$host='localhost';
+$db = '';
+$username = '';
+$password = '';
+// SMS Configuration
+$smstoken = '';
+// Email Configuration
+$emailHost = '';
+$emailUsername = '';
+$emailPassword = '';
+$SMTPDebug = 0;
+$SMTPAuth = true;
+$SMTPSecure = 'ssl';
+$Port = 465;
+// API credentials from https://login.sendpulse.com/settngs/#api
+$apiUserId = '';
+$apiSecret = '';
+// define('PATH_TO_ATTACH_FILE', __FILE__);
+// paystack credentials
+// $paystackKey = '';
